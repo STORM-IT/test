9d4bd97aae46fa17622bf8e791879d00645c7a63
ali <stooormix@gmail.com>
2021-09-11 00:36:23 +0430

    nav

--- jquery-cod-test.js
+++ jquery-cod-test.js
@@ -1,3 +1,46 @@
-$(document).ready(function () {
+  $(document).ready(function () {
+    $(".ul-menu li").hover(function (e) {
+      let id = $(this).attr('id');
+      var ul;
+      switch (id) {
+        case "Foreign":
+          ul = document.getElementById("list-nav-film-Foreign");
+          // alert(ul);
+
+          $(ul).stop().slideDown(600);
           
-});
+          $(ul).hover(function () {
+              // over
+              console.log("22");
+              $(ul).stop().slideDown(600);
+            }, function () {
+              // out
+              console.log("33");
+               $(ul).stop().slideUp(600);
+            }
+          );
+          
+          
+          break;
+        case "parsian":
+
+          break;
+        case "Bests":
+
+          break;
+        case "years":
+
+          break;
+
+        default:
+          break;
+      }
+
+    }, function () {
+      $("#list-nav-film-Foreign").stop().slideUp(500);
+      //$(".ul-menu ul").stop().slideUp(500);
+      console.log("11");
+      // out
+    });
+
+  });
